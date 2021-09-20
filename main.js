d232bf39778957c7bae6b5f840f1d8f3fa6ea1ab
Nissrine Canina <niss.canina@gmail.com>
2021-09-20 15:30:58 -0400

    Completed Assignment

--- main.js
+++ main.js
@@ -3,6 +3,64 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const likeHearts = document.querySelectorAll(".like-glyph");
+for (const like of likeHearts) {
+  
+  like.addEventListener ('click', function(event) {
+    const heart = event.target;
+  
+    if (heart.innerText === EMPTY_HEART) {
+      likeBack(event, heart);
+    } else {
+      
+      dislikeBack(event, heart);
+    }
+  })
+}
+
+function likeBack(event, heart) {
+     
+        mimicServerCall()
+        .then(function() {
+          heart.innerText = FULL_HEART;
+          heart.classList.add("activated-heart", "like-glyph");
+          })
+        .catch(function(error) {
+        
+          document.querySelector(".hidden").classList.remove("hidden");
+
+          alert("Random server error. Try again.");
+          document.querySelector("#modal-message").innerHTML = "Random server error. Try again.";
+
+          setTimeout(function() {
+            const modalDiv = document.querySelector("#modal")
+            modalDiv.className = "hidden";
+          }, 3000);
+      })
+} 
+
+function dislikeBack(event, heart) {
+
+    mimicServerCall()
+    .then(function() {
+      heart.innerText = EMPTY_HEART;
+      heart.classList.remove("activated-heart");
+      })
+}
+
+
+
+
+    
+
+
+
+    
+
+
+
+
+
 
 
 
